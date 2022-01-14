99a9f120ede8a966e4678ecd3c16d4528a949f88
Jerry Majors Patterson <92124939+jearldean@users.noreply.github.com>
2022-01-14 04:57:55 +0000

    Day 4 complete

--- crud.py
+++ crud.py
@@ -49,6 +49,9 @@ def create_user(email, password):
 
     user = User(email=email, password=password)
 
+    db.session.add(user)
+    db.session.commit()
+    
     return user
 
 def get_users():
@@ -56,11 +59,39 @@ def get_users():
 
     return User.query.all()
 
+
 def get_user_by_id(user_id):
     """Return a user by primary key."""
 
     return User.query.get(user_id)
-    
+
+
+def get_user_by_email(email):
+    """Return a user by email."""
+
+    return User.query.filter(User.email == email).first()
+
+
+def does_this_user_exist_already(email):
+    """Return a boolean we can use for the if-statement in server.py."""
+
+    if get_user_by_email(email):
+        return True
+    else:
+        return False
+
+
+def does_the_password_match(email, password):
+    """Return a boolean we can use for the if-statement in server.py."""
+
+    if User.query.filter(User.email == email).first() and (
+        User.query.filter(User.password == password).first()):
+            return True
+            # return User.query.filter(User.email == email).first()
+    else:
+        return False
+
+
 def create_movie(title, overview, release_date=None, poster_path=None):
     """Create and return a new movie.
     
--- server.py
+++ server.py
@@ -18,7 +18,66 @@ def homepage():
 
     return render_template('homepage.html')
 
+    
+@app.route("/users", methods=["POST"])
+def register_user():
+    """Create a new user."""
 
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not crud.does_this_user_exist_already(email):
+        crud.create_user(email, password)
+        flash("Account created! Please log in.")
+    else:
+        flash("This user already exists! Please try again")
+    
+    return redirect("/")
+
+
+@app.route("/login", methods=["POST"])
+def login_user():
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if crud.does_the_password_match(email, password):
+        flash("Logged in! Please rate some movies!")
+
+        # Establish a session only after a successful login:
+        if "session_details" not in session:
+            session["session_details"] = {}
+
+        # Override on each new login:
+        session["session_details"]["user_email"] = email
+        # This failed when we didn't initialize the movie_id key:
+        session["session_details"]["movie_id"] = None
+        
+        return redirect("/movies")
+    else:
+        flash("Password mismatch! Please try again")
+        return redirect("/")
+
+
+@app.route("/rate", methods=["POST"])
+def rate_movie():
+
+    rating = request.form.get("rating")  # Any form reply back is a string.
+    try:
+        rating = int(rating)  # Coerce to int.
+    except TypeError:
+        flash("Please try again with a number from 1-5.")
+
+    user_email = session["session_details"]["user_email"]
+    user = crud.get_user_by_email(user_email)
+    movie = crud.get_movie_by_id(session["session_details"]["movie_id"])
+
+    crud.create_rating(user, movie, score=rating)
+
+    flash("Rate more movies!")
+    return redirect("/movies")
+
+    
 @app.route("/movies")
 def all_movies():
     """View all movies."""
@@ -31,6 +90,9 @@ def one_movie(movie_id):
     """View one movie."""
 
     movie = crud.get_movie_by_id(movie_id)
+
+    # Put movie_id in session here: (override on each new page load)
+    session["session_details"]["movie_id"] = movie_id
 
     return render_template('movie_details.html', movie=movie)
 
@@ -47,6 +109,7 @@ def one_user(user_id):
     """View one user."""
 
     user = crud.get_user_by_id(user_id)
+    print(f"I am user_id: {user_id}")
 
     return render_template('user_details.html', user=user)
 
@@ -56,3 +119,22 @@ if __name__ == "__main__":
     # DebugToolbarExtension(app)
 
     app.run(host="0.0.0.0", debug=True)
+
+
+# from flask import request, session, flash, redirect
+
+# @app.route('/handle-login', methods=['POST'])
+# def handle_login():
+#     """Log user into application."""
+
+#     username = request.form['username']
+#     password = request.form['password']
+
+#     if password == 'let-me-in':   # FIXME
+#         session['current_user'] = username
+#         flash(f'Logged in as {username}')
+#         return redirect('/')
+
+#     else:
+#         flash('Wrong password!')
+#         return redirect('/login')
